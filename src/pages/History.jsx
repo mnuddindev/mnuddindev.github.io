bf18814bd14e0d03b84d2ce42a2f37c3f04d392b
mnudindev <inadislam@gmail.com>
2023-12-30 19:01:43 +0600

    ✨ Animation Added to HIstory page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,11 +1,34 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../components/History/PageTitle'
 import Timeline from '../components/History/Timeline'
 import gsap from 'gsap'
 
 const History = () => {
+  const animationMap = [
+    { revealedDots: 8, item: '.psc' },
+    { revealedDots: 40, item: '.jsc' },
+    { revealedDots: 63, item: '.ssc' },
+    { revealedDots: 81, item: '.hsc' },
+    { revealedDots: 95, item: '.microsoft-course' },
+    { revealedDots: 119, item: '.appifylab' },
+    { revealedDots: 134, item: '.bkttc' },
+  ];
+  let dotTimeline
   const animationCompleted = useRef(false)
-
+  const dotanimation = () => {
+    const dots = document.querySelectorAll('.timeline-dot')
+    dotTimeline = gsap.timeline()
+    dots.forEach((dot, index) => {
+      dotTimeline.fromTo(dot, { opacity: 0, x: '-100%' }, { opacity: 1, x: '0%', duration: 0.06, ease: 'power1.inOut' }, index * 0.06);
+      const animationItem = animationMap.find(item => item.revealedDots === index)
+      if (animationItem) {
+        dotTimeline.add(() => {
+          gsap.fromTo(animationItem.item, {x: '-100%', opacity: 0}, {x: '0%', opacity: 1})
+        })
+      }
+    })
+    return dotTimeline
+  }
   const titleAnimation = (params) => {
     const { h1, h2 } = params;
     const tl = gsap.timeline({
@@ -18,23 +41,30 @@ const History = () => {
         animationCompleted.current = true;
       },
     });
-    tl.fromTo(h2, { x: '-100%' }, { opacity: 1, x: '0%', duration: 1, ease: 'power2.out'}, 'race').fromTo(h1, { x: '-100%' }, { opacity: 1, x: '0%', duration: 0.7, ease: 'power2.out'}, 'race');
+    tl.fromTo(h2, { x: '-100%' }, { opacity: 1, x: '0%', duration: 1, ease: 'power2.out' }, 'race').fromTo(h1, { x: '-100%' }, { opacity: 1, x: '0%', duration: 0.7, ease: 'power2.out' }, 'race');
     return tl;
-  };
+  }
 
   useEffect(() => {
     const history = document.querySelector('.history');
-    const [h2, h1] = document.querySelectorAll('.history .container-fluid .page-titles h2, .history .container-fluid .page-titles h1');
+    const [h2, h1] = document.querySelectorAll('.history .container-fluid .page-titles h2, .history .container-fluid .page-titles h1')
+    const title = titleAnimation({ h1, h2 })
+    dotTimeline = dotanimation()
 
-    const title = titleAnimation({ h1, h2 });
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             if (animationCompleted.current) {
               title.restart();
+              dotTimeline.restart()
+              animationCompleted.current = false
+              animationMap.map((k, v) => {
+                gsap.set(k.item, {  x: -1000, opacity: 0 })
+              })
             } else {
-              title.play();
+              title.play()
+              dotTimeline.play()  
             }
           }
         });
@@ -52,6 +82,8 @@ const History = () => {
       if (history) {
         observer.unobserve(history);
       }
+      animationCompleted.current = false;
+      dotTimeline.kill()
     };
   }, []);
 
